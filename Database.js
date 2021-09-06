0c8d798761bd5f23f26a92d0dd7965f466036a93
Niharika More <47314684+niharika1000@users.noreply.github.com>
2021-09-06 17:44:01 +0530

    Add files via upload
    5th commit

--- Database.js
+++ Database.js
@@ -1,5 +1,9 @@
 const Pool = require('pg').Pool;
 const Yup = require("yup");
+const express = require('express');
+const app = express();
+const url= require('url');
+app.use(express.urlencoded({ extended: false }));
 const pool = new Pool({
   user: 'niharika',
   host: '127.0.0.1',
@@ -10,9 +14,9 @@ const pool = new Pool({
 
 const linkSchema = Yup.object({
   body: Yup.object().shape({
-    Username: Yup.string().min(8).required(),
-    Password: Yup.string().min(8).max(32).matches(/^[^\s]*$/,"Password should not contain whitespace").required(),
-    Email: Yup.string().email("E-mail address not valid").required(),
+    Username: Yup.string().min(8),
+    Password: Yup.string().min(8).max(32).matches(/^[^\s]*$/,"Password should not contain whitespace"),
+    Email: Yup.string().email("E-mail address not valid"),
     Gender:Yup.string(),
     ProfilePic:Yup.object()
   })
@@ -28,34 +32,43 @@ const validate = (schema) => async (request,response,next) =>
   }
 }; 
 
-const createUser = (validate(linkSchema) ,( request, response) => {
-      const {Username,Password,Email,Gender,ProfilePic} = request.body;
+const createUser = (validate(linkSchema),(request, response) => {
+      console.log(request.body);
+      Body=request.body;
+      //linkSchema.validate(Body);
+      const {Username,Password,Email,Gender,ProfilePic} = Body;
       pool.query(`select * from public."UINFO" where "Username"= $1`,[Username], (error, results)=>
     {   
       if (results.rows.length==0){ 
         pool.query(`INSERT INTO public."UINFO"("Username", "Password", "Email", "Gender", "ProfilePic")
         VALUES ($1, $2, $3, $4, $5);`, [ Username,Password,Email,Gender,ProfilePic],(error,results)=>
-        { if (error) {throw error}  console.log("Account created succesfully") })
+        { if (error) {throw error}  response.send("Account created succesfully") })
       }
-      else { console.log("Username already exists"); response.redirect('./signin.html'); } 
+      else { response.send("Username already exists"); }
       response.status(200); 
     });
-  }
+  });
   
 
-const user = ('/api/signin', (request, response) => {
+const user = ((request, response) => {
   const {Username,Password}=request.body;
+  console.log(Username, Password);
     pool.query('select * from public."UINFO" where "Username"=$1 and "Password"=$2',[Username,Password],(error, results) => {
       if (error) {throw error}
       if (results.rows.length==1){
-        response.redirect('profile.html');
+        response.redirect(url.format({
+          pathname:'http://localhost:3000/profile',
+          query:{"Username": Username }
+        }));
       }
+      else{ response.redirect('http://localhost:3000/login'); }
+
+      }); 
       response.status(200);
       
   });
-  });
-  const userprofile = ('/api/profile/:Username', (request, response) => {
-    const para= request.params.Username;
+  const userprofile = ( (request, response) => {
+    //const para= request.params.Username;
     pool.query(`select * from public."UINFO" where "Username"=$1`,[para],(error, results) => {
       if (error) {
         throw error
