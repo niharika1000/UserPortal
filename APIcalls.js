8ef9555eb8f102f6395d0048194e6bab1ade7475
Niharika More <47314684+niharika1000@users.noreply.github.com>
2021-09-07 14:19:08 +0530

    Add files via upload

--- APIcalls.js
+++ APIcalls.js
@@ -2,18 +2,43 @@ const express = require('express');
 const path=require('path');
 const app = express();
 const port = 3000;
-
-
-app.use(express.json());
-app.get('/', (request, response) => {
-    response.json({ info: 'Welcome to Digital School' });
-  });
-
+const router = express.Router();
 const db = require('./Database.js');
+app.use(express.urlencoded({ extended: false }));
 
-app.post('/api/RegisterUser', db.createUser);
-app.post('/api/signin', db.user);
-app.get('/api/profile/:Username', db.userprofile);
+const jwt = require("jsonwebtoken");
+
+const config = process.env;
+
+
+const auth = require("./middleware/auth");
+
+app.post("/welcome", auth, (req, res) => {
+  res.status(200).send("Welcome 🙌 ");
+});
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+router.get('/login',function(req,res){
+  res.sendFile(path.join(__dirname+'/views/signin.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get('/profile',function(req,res){
+  res.sendFile(path.join(__dirname+'/views/profile.html'));
+});
+
+router.get('/register',function(req,res){
+  res.sendFile(path.join(__dirname+'/views/signup.html'));
+});
+
+//add the router
+app.use('/', router);
+
+app.post('/register', db.createUser);
+app.post('/login', db.user);
+app.get('/api/profile', db.userprofile);
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`);
--- Database.js
+++ Database.js
@@ -1,5 +1,17 @@
 const Pool = require('pg').Pool;
 const Yup = require("yup");
+const express = require('express');
+const app = express();
+const url= require('url');
+app.use(express.urlencoded({ extended: false }));
+const jwt = require("jsonwebtoken");
+
+const config = process.env;
+
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const pool = new Pool({
   user: 'niharika',
   host: '127.0.0.1',
@@ -10,9 +22,9 @@ const pool = new Pool({
 
 const linkSchema = Yup.object({
   body: Yup.object().shape({
-    Username: Yup.string().min(8).required(),
-    Password: Yup.string().min(8).max(32).matches(/^[^\s]*$/,"Password should not contain whitespace").required(),
-    Email: Yup.string().email("E-mail address not valid").required(),
+    Username: Yup.string().min(8),
+    Password: Yup.string().min(8).max(32).matches(/^[^\s]*$/,"Password should not contain whitespace"),
+    Email: Yup.string().email("E-mail address not valid"),
     Gender:Yup.string(),
     ProfilePic:Yup.object()
   })
@@ -28,38 +40,73 @@ const validate = (schema) => async (request,response,next) =>
   }
 }; 
 
-const createUser = (validate(linkSchema) ,( request, response) => {
-      const {Username,Password,Email,Gender,ProfilePic} = request.body;
-      pool.query(`select * from public."UINFO" where "Username"= $1`,[Username], (error, results)=>
-    {   
+const createUser = (validate(linkSchema),(request, response) => {
+      console.log(request.body);
+      Body=request.body;
+      //linkSchema.validate(Body);
+      const {Username,Password,Email,Gender,ProfilePic} = Body;
+
+      if(Username.length<8){ response.status(401).send("Username length must be greater than 8"); return; }
+      if( (/\s/g).test(Username)){ response.status(401).send("Username must not have a space and any special characters"); return; } 
+      if( (/\s/g).test(Password)){ response.status(401).send("Password must not have a space or a special character"); return; }
+      if(  Password.length<8 || Password.length>32){ response.status(401).send("Password length must be greater than 8 and less than 32"); return; }
+      if(! (/^[\w]*@[a-zA-Z]*\.[a-z]*$/g).test(Email)){ response.status(401).send("Invalid Email"); return; }
+      if(! (/^male$|^female$|^others$/gi).test(Gender)){ response.status(401).send("Gender be male, female, or others"); return; } 
+      
+      const Passhash = bcrypt.hashSync(Password,saltRounds);
+      console.log(Passhash);
+
+      pool.query(`select * from public."UINFO" where "Email"= $1`,[Email], (error, results)=>
+    {  
       if (results.rows.length==0){ 
         pool.query(`INSERT INTO public."UINFO"("Username", "Password", "Email", "Gender", "ProfilePic")
-        VALUES ($1, $2, $3, $4, $5);`, [ Username,Password,Email,Gender,ProfilePic],(error,results)=>
-        { if (error) {throw error}  console.log("Account created succesfully") })
+        VALUES ($1, $2, $3, $4, $5);`, [ Username,Passhash,Email,Gender,ProfilePic],(error,results)=>
+        { if (error) {throw error}  response.send("Account created succesfully") })
       }
-      else { console.log("Username already exists"); response.redirect('./signin.html'); } 
+      else { response.send("Username already exists"); }
       response.status(200); 
     });
-  }
+  });
   
 
-const user = ('/api/signin', (request, response) => {
-  const {Username,Password}=request.body;
-    pool.query('select * from public."UINFO" where "Username"=$1 and "Password"=$2',[Username,Password],(error, results) => {
+const user = ((request, response) => {
+  const {Email,Password}=request.body;
+  //const Passhash = bcrypt.hashSync(Password,saltRounds);
+  //console.log(Passhash1);
+  //result = bcrypt.compareSync(Password,Passhash2);
+  //bcrypt.compare(Password, Passhash)
+    pool.query('select * from public."UINFO" where "Email"=$1',[Email],(error, results) => {
       if (error) {throw error}
-      if (results.rows.length==1){
-        response.redirect('profile.html');
+      //console.log(results.rows[0].Email);
+      if (results.rows.length==1 && bcrypt.compareSync(Password,results.rows[0].Password)){
+
+       /* const token = jwt.sign(
+          { user_id: user._id, Email },
+          process.env.TOKEN_KEY,
+          {
+            expiresIn: "2h",
+          }
+        );
+  */
+        // save user token
+
+        response.redirect(url.format({
+          pathname:'http://localhost:3000/profile',
+          query:{
+                  "Email":results.rows[0].Email }
+        }));
       }
+      else{ response.redirect('http://localhost:3000/login'); }
+
+      }); 
       response.status(200);
       
   });
-  });
-  const userprofile = ('/api/profile/:Username', (request, response) => {
-    const para= request.params.Username;
-    pool.query(`select * from public."UINFO" where "Username"=$1`,[para],(error, results) => {
-      if (error) {
-        throw error
-      }
+  const userprofile = ( (request, response) => {
+    const { Password , Email}= request.query;
+    pool.query(`select * from public."UINFO" where "Email"=$1`,[Email],(error, results) => {
+      if (error) { throw error}
+      if(bcrypt.compareSync(Password,results.rows[0].Password))
       response.status(200).json(results.rows);
     });
   });
